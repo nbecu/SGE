b8ae82471248dbb1e5aba7c4d6415708eb653241
Neraliel <119414220+Neraliel@users.noreply.github.com>
2023-02-17 10:57:37 +0100

    in progress : add a player legend

--- Examples/A to Z examples/example2.2.py
+++ Examples/A to Z examples/example2.2.py
@@ -7,8 +7,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 monApp=QtWidgets.QApplication([])
 
-myModel=SGModel(860,700, windowTitle="My first simulation/game")
-# You can add a title to the main window of your simulation/game
+myModel=SGModel(860,700, windowTitle="About legend (3)")
 
 aGrid=myModel.createGrid(10,10,"square",size=60, gap=2)
 aGrid.setValueForCells({"landUse":"grass"})
@@ -20,12 +19,12 @@ myModel.setUpPov("povLandUse",{"landUse":{"grass":Qt.green,"shrub":Qt.yellow,"fo
 myModel.setUpPov("povLandUse2",{"landUse":{"grass":Qt.green,"shrub":Qt.green,"forest":Qt.darkGreen}})
 
 theFirstLegend=myModel.createLegendAdmin()
-# the admin's legend enables to change all the entities without restrictions
-# it will be displayed only for an Admin user.
-# By default, a model user is an Admin user
-# To specify another user status add the instruction -> myModel.iAm("Name of the player")
-# To come back to Admin user status, type the instruction -> myModel.iAm("Admin")
+theFirstLegend=myModel.createLegendAdmin()
+myModel.createPlayer("Player 1")
+myModel.createLegendForPlayer('Player 1 Legend','povlandUse','Player 1')
+# to have the Player 1 view 
 myModel.iAm("Name of the player")
+# To come back to Admin user status, type the instruction -> myModel.iAm("Admin")
 myModel.iAm("Admin")
 
 myModel.launch_withoutMqtt() 
