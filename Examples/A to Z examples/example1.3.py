e10173db51d06e729e9cb1760a0ee01b8df91456
Neraliel <119414220+Neraliel@users.noreply.github.com>
2023-02-20 14:38:58 +0100

    first commit

---
+++ Examples/A to Z examples/example1.3.py
@@ -0,0 +1,26 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+from mainClasses.SGModel import SGModel
+from PyQt5 import QtWidgets 
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+monApp=QtWidgets.QApplication([])
+
+myModel=SGModel(1530,830, windowTitle="About pov (3)")
+
+aGrid=myModel.createGrid(10,10,"square",Qt.gray,size=75)
+aGrid.setValueForCells({"landUse":"grass"})
+aGrid.setForX({"landUse":"forest"},1)
+aGrid.setForX({"landUse":"forest"},2)
+aGrid.setForRandom({"landUse":"shrub"},10)
+
+#In this example there are two pov: povLandUse and povLandUse2
+# povLandUse can see the difference between grass and schrub, poveLandUse2 cannot
+myModel.setUpPov("povLandUse",{"landUse":{"grass":Qt.green,"shrub":Qt.yellow,"forest":Qt.darkGreen}},[aGrid])
+myModel.setUpPov("povLandUse2",{"landUse":{"grass":Qt.green,"shrub":Qt.green,"forest":Qt.darkGreen}},[aGrid])
+
+myModel.launch_withoutMqtt() 
+
+sys.exit(monApp.exec_())
