f0f7db24503cb96af15a9d053ab2988d60dc3012
Neraliel <119414220+Neraliel@users.noreply.github.com>
2023-04-04 15:43:31 +0200

    Update example5.0.py

--- Examples/A to Z examples/example5.0.py
+++ Examples/A to Z examples/example5.0.py
@@ -11,22 +11,22 @@ theFirstGrid=myModel.createGrid(10,10,"hexagonal",Qt.gray, name="basicGrid")
 myModel.setUpEntityValueAndPov("Forester",{"Forest":{"Niv1":Qt.yellow,"Niv2":Qt.red,"Niv3":Qt.green},"sea":{"deep sea":Qt.blue,"reasonable":Qt.cyan}},[theFirstGrid],"sea","reasonable")
 myModel.setInitialPov("Forester")
 
-theFirstGrid.setForRandom({"Forest":"Niv1"},30)
-theFirstGrid.setForRandom({"Forest":"Niv2"},4)
+theFirstGrid.setRandomCells("Forest","Niv1",30)
+theFirstGrid.setRandomCells("Forest","Niv2",4)
 
 anAgentLac=myModel.newAgent("lac","circleAgent",[theFirstGrid])
-myModel.setUpEntityValueAndPov("Forester",{"boat":{"new":Qt.blue,"old":Qt.cyan}},"lac","boat","old",[theFirstGrid])
+#myModel.setUpEntityValueAndPov("Forester",{"boat":{"new":Qt.blue,"old":Qt.cyan}},"lac","boat","old",[theFirstGrid])
 theFirstLegend=myModel.createLegendAdmin()
 
 # Agents can be also placed by a player
 # After calling your player and set time à 0
-thePlayer=myModel.createPlayer("Player1")
-myModel.timeManager.addGamePhase("theFirstPhase",thePlayer)
+#thePlayer=myModel.createPlayer("Player1")
+#myModel.timeManager.addGamePhase("theFirstPhase",thePlayer)
 
 # you can create a GameAction for your player to interact with the grid
 # it also permits to give interaction between cell and agents like here : if you place a certain agent on a certain type of cell, the cell updates to a new status
 numberofAction=7
-thePlayer.addGameAction(myModel.createCreateAction(anAgentLac,numberofAction,{"boat":["old"]},[lambda aCell: aCell.checkValue({"sea":"reasonable"})]  ,  [lambda aCell: aCell.changeValue({"sea": "deep sea"})]  , [lambda aCell: aCell.parent.getCellFromCoordinates(1,1).checkValue({"sea":"reasonable"}) ]))
+#thePlayer.addGameAction(myModel.createCreateAction(anAgentLac,numberofAction,{"boat":["old"]},[lambda aCell: aCell.checkValue({"sea":"reasonable"})]  ,  [lambda aCell: aCell.changeValue({"sea": "deep sea"})]  , [lambda aCell: aCell.parent.getCellFromCoordinates(1,1).checkValue({"sea":"reasonable"}) ]))
 
 myModel.iAm("Player1")
 myModel.show() 
