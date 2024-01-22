7fd94f211a9dfb7a528c3ee7b67a3d4fb6872e42
nbecu <nicolas.becu@gmail.com>
2024-01-22 22:13:14 +0100

    example de simulation pour tester les graphs

--- Examples/A to Z examples/exProductionAndConsumption.py
+++ Examples/A to Z examples/exProductionAndConsumption.py
@@ -59,21 +59,23 @@ for aVal in list(range(0,110,10)):
 Consumers.newPov("energy","energy",aDict)
 
     # Define methods
+def move(aConsumer):
+    dest = sorted(list(aConsumer.cell.getNeighborCells()),  key=lambda x:x.value('energy'))[-1]
+    return aConsumer.moveTo(dest)
+
+def moveAndConsume(aConsumer):
+    consume(move(aConsumer))
 
 def consume(aConsumer):
     if aConsumer.value('strategy') == 'unrestrained':
         consumptionPercentage  = 0.8
     if aConsumer.value('strategy') == 'restrained':
         consumptionPercentage  = 0.3
-
     qtConsume = (round((aConsumer.cell.value('energy') * consumptionPercentage)/10))*10
     aConsumer.incValue('energy',qtConsume,100)
     aConsumer.cell.decValue('energy',qtConsume,0)
     if aConsumer.cell.value('energy') < 10:
         score.decValue(1)
-
-
-
 
 def reproAndDie(aConsumer):
     if aConsumer.value('energy') == 100:
@@ -81,21 +83,79 @@ def reproAndDie(aConsumer):
         aConsumer.setValue('energy',50)
         score.incValue(1)
         return
-    aConsumer.decValue('energy',10,0)
-    if aConsumer.value('energy') <=40:
+    aConsumer.decValue('energy',20,0)
+    if aConsumer.value('energy') <=20:
+        print('del: '+str(aConsumer.id))
         Consumers.deleteEntity(aConsumer)
         score.decValue(1)
 
+## Define Regulator
+Regulators=myModel.newAgentSpecies("Regulator","circleAgent")
+Regulators.setValue("satisf constant",0)
+Regulators.setValue("satisf not constant",0)
+Regulators.newAgentsAtRandom(5,ProductionUnits)
+
+
+    # Define methods
+def evaluate(aRegulator):
+    aCell = aRegulator.cell
+    if aCell.value('production system') == 'boosted' and aCell.value('energy') == 100:
+        if aRegulator.entDef.value("satisf constant") == aRegulator.entDef.value("satisf not constant"):
+            if random.random() < 0.5:
+                newS = 'constant'
+            else :
+                newS = 'not constant'
+        else :
+            ratio = aRegulator.entDef.value("satisf constant") / (aRegulator.entDef.value("satisf constant") + aRegulator.entDef.value("satisf not constant"))
+            if random.random < ratio :
+                newS = 'constant'
+            else :
+                newS = 'not constant'
+            aCell.setValue('production system',newS)
+
+    if aCell.value('production system') == 'constant' and aCell.value('energy') == 100:
+        aRegulator.classDef.incValue("satisf constant",1)
+    if aCell.value('production system') == 'not constant' and aCell.value('energy') == 100:
+        aRegulator.classDef.incValue("satisf not constant",1)
+    if aCell.value('production system') == 'constant' and aCell.value('energy') <= 10:
+        aRegulator.classDef.decValue("satisf constant",1)
+        aRegulator.cell.setValue('production system','boosted')
+    if aCell.value('production system') == 'not constant' and aCell.value('energy') <= 10:
+        aRegulator.classDef.decValue("satisf not constant",1)
+        aRegulator.cell.setValue('production system','boosted')
+
+
+def evaluateCell(aCell):
+    if (aCell.value('production system') == 'boosted') and (aCell.value('energy') >= 50):
+        if Regulators.value("satisf constant") == Regulators.value("satisf not constant"):
+            if random.random() < 0.5:
+                newS = 'constant'
+            else :
+                newS = 'not constant'
+        else :
+            ratio = (Regulators.value("satisf constant")+100) / (Regulators.value("satisf constant") +200+ Regulators.value("satisf not constant"))
+            if random.random() < ratio :
+                newS = 'constant'
+            else :
+                newS = 'not constant'
+            aCell.setValue('production system',newS)
 
 
 ## Model actions 
 
 modelAction1=myModel.newModelAction_onCells(lambda aPU: produce(aPU))
-modelAction2=myModel.newModelAction(lambda : Consumers.moveRandomly())
-modelAction3=myModel.newModelAction_onAgents("Consumer",(lambda aConsumer: consume(aConsumer)))
+# modelAction2=myModel.newModelAction(lambda : Consumers.moveRandomly())
+# modelAction2=myModel.newModelAction_onAgents("Consumer",(lambda aConsumer: move(aConsumer)))
+# modelAction3=myModel.newModelAction_onAgents("Consumer",(lambda aConsumer: consume(aConsumer)))
+modelAction2=myModel.newModelAction_onAgents("Consumer",(lambda aConsumer: moveAndConsume(aConsumer)))
 modelAction4=myModel.newModelAction_onAgents("Consumer",(lambda aConsumer: reproAndDie(aConsumer)))
+modelAction5=myModel.newModelAction(lambda : Regulators.moveRandomly())
+modelAction6=myModel.newModelAction_onAgents("Regulator",(lambda aRegulator: evaluate(aRegulator)))
 
-myModel.timeManager.newModelPhase([modelAction1,modelAction2,modelAction3,modelAction4])
+modelAction7=myModel.newModelAction_onCells((lambda aCell: evaluateCell(aCell)))
+
+
+myModel.timeManager.newModelPhase([modelAction1,modelAction2,modelAction4,modelAction5,modelAction6,modelAction7])
 
 
 ## Define other interface widgets
